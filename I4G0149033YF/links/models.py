525ca8c52fa9bcadf975e75ffeccadaae436b022
John-ChuOnyido <onyidojohn@gmail.com>
2022-07-21 10:04:15 +0100

    made changes

--- I4G0149033YF/links/models.py
+++ I4G0149033YF/links/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from .models import rest_framework
+from .models import ActiveLinkManager
 from django.contrib.auth import get_user_model
 from django.utils import timezone
 from django.urls import reverse
@@ -13,7 +14,7 @@ class Link(models.Model):
     description = models.CharField(max_length=200)
     identifier = models.SlugField(max_length=20, blank = True, unique=True)
     author = models.ForeignKey(
-        get_user_model(), on_delete=models.CASCADE, related_name="blog_posts"
+    get_user_model(), on_delete=models.CASCADE, related_name="blog_posts"
     )
     
     created_date = models.DateTimeField(auto_now_add=True)
